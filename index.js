c58b32a56d97fc430960e437426b2f054e77bb2a
Ainara Saralegui <ainarasaal@gmail.com>
2018-05-08 14:04:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"]
 
-function destructivelyAppendKitten(kittens,name) {
+function destructivelyAppendKitten(name) {
   kittens.push(name)
   return kittens
 }
